693ca808e0f13a6ff87a2bcb345de197bb6fdcc3
saron-t <stekie@uncc.edu>
2016-10-11 21:31:39 +0000

    Done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,22 +1,14 @@
 # This file is app/controllers/movies_controller.rb
 class MoviesController < ApplicationController
   
-  def index
-    @movies = Movie.all
-  end
-  
-
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.html.haml by default
   end
 
-  # add below all other methods
-    private
-
-  def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  def index
+    @movies = Movie.all
   end
   
   def new
@@ -49,6 +41,13 @@ class MoviesController < ApplicationController
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
+  end
+  
+    # add below all other methods
+    private
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
   
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,32 +1,11 @@
 class Movie < ActiveRecord::Base
-    ####  Create
-    starwars = Movie.create!(:title => 'Star Wars',
-        :release_date => '25/4/1977', :rating => 'PG')
-    # note that numerical dates follow European format: dd/mm/yyyy
-    requiem =  Movie.create!(:title => 'Requiem for a Dream',
-        :release_date => 'Oct 27, 2000', :rating => 'R')
-    #  Creation using separate 'save' method, used when updating existing records
-    field = Movie.new(:title => 'Field of Dreams',
-        :release_date => '21-Apr-89', :rating => 'PG')
-    field.save!
-    field.title = 'New Field of Dreams'
-    ####  Read
-    pg_movies = Movie.where("rating = 'PG'")
-    ancient_movies = Movie.where('release_date < :cutoff and rating = :rating',
-        :cutoff => 'Jan 1, 2000', :rating => 'PG')
-    ####  Another way to read
-    Movie.find(3)  # exception if key not found
-    ####  Update
-    starwars.update_attributes(:description => 'The best space western EVER',
-        :release_date => '25/5/1977')
-    requiem.rating = 'NC-17'
-    requiem.save!
-    ####  Delete
-    requiem.destroy
-    Movie.where('title = "Requiem for a Dream"')
-    ####  Find returns an enumerable
-    Movie.where('rating = "PG"').each do |mov|
-      mov.destroy
-end
+
+	def Movie.get_all_ratings
+		all_ratings = []
+		self.select(:rating).group(:rating).each do |mo|
+			all_ratings << mo.rating
+		end
+		return all_ratings
+	end
 
 end
